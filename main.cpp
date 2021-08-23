2adf46ad5f0529318a62da99979ccb385b1795e9
Lucas.B <62809823+lucasb1993955@users.noreply.github.com>
2021-08-22 22:30:08 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-using namespace std;
 #include<ctime>
 #include<cstring>
+#include"include/Cancha.h"
 #include "include/Menu.h"
 #include"include/rlutil.h"
+#include"include/usuario.h"
+
+using namespace std;
 
 int main(){
+rlutil::setBackgroundColor(rlutil::DARKGREY);
 setlocale(LC_ALL, "spanish");
 menuPrincipal();
 
